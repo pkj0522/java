704e32129d6627e690d91db135922469230c9507
USER <USER@AMP-15>
2019-04-25 12:10:17 +0900

    commit

---
+++ chap11-ex14/src/SliderEx.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SliderEx extends JFrame{
+	public SliderEx() {
+		setTitle("¹Ú°æÁ¦");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 200, 100);
+		slider.setPaintLabels(true);
+		slider.setPaintTicks(true);
+		slider.setPaintTrack(true);
+		slider.setMajorTickSpacing(50);
+		slider.setMajorTickSpacing(10);
+		c.add(slider);
+		
+		setSize(300, 100);
+		setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {
+		new SliderEx();
+	}
+
+}
