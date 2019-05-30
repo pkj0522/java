364865279c0d06b21da22448b49e75701cf1f29f
USER <USER@AMP-02>
2019-05-30 14:44:18 +0900

    commit

--- chap13-ex08/src/TabAndThreadEx.java
+++ chap13-ex08/src/TabAndThreadEx.java
@@ -1,41 +1,113 @@
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.util.function.*;
 
-import javax.swing.JFrame;
+class MyLabel extends JLabel{
+	int barSize = 0;
+	int maxBarSize;
+	
+	MyLabel(int maxBarSize){
+		this.maxBarSize = maxBarSize;
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.MAGENTA);
+		int width = (int)(((double)(this.getWidth()))/maxBarSize*barSize);
+		if(width == 0)
+			return;
+		g.fillRect(0, 0, width, this.getHeight());
+	}
+	
+	synchronized void fill(){
+		if(barSize == maxBarSize){
+			try{
+				wait();
+			}
+			catch(InterruptedException e){
+				return;
+			}
+		}
+		barSize++;
+		repaint();
+		notify();
+	}
+	
+	synchronized void consume(){
+		if(barSize == 0){
+			try{
+				wait();
+			}
+			catch(InterruptedException e){
+				return;
+			}
+		}
+		barSize--;
+		repaint();
+		notify();
+	}
 
-public class TabAndThreadEx extends JFrame {
+}
 
+class ConsumerThread extends Thread{
+	MyLabel bar;
+	
+	public ConsumerThread(MyLabel bar) {
+		this.bar = bar;
+	}
+	
+	public void run(){
+		while(true){
+			try{
+				sleep(200);
+				bar.consume();
+			}
+			catch(InterruptedException e){
+				return;
+			}
+		}
+	}
+}
+
+
+public class TabAndThreadEx extends JFrame{
+	
 	MyLabel bar = new MyLabel(100);
-	TabAndThreadEx(String title) {
+	
+	TabAndThreadEx(String title){
 		super(title);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(null);
 		bar.setBackground(Color.ORANGE);
 		bar.setOpaque(true);
-		bar.setLocation(20, 30);
+		bar.setLocation(20, 50);
 		bar.setSize(300, 20);
 		c.add(bar);
 		
 		c.addKeyListener(new KeyAdapter() {
-			public void keyPressed(KeyEvent e)
-			{
+			public void keyPressed(KeyEvent e){
 				bar.fill();
 			}
+			
 		});
-		setSize(350, 200);
+		
+		setSize(350,200);
 		setVisible(true);
 		
 		c.requestFocus();
-		ConsumerThread th = new
-				ConsumerThread(bar);
-		th.start(); // 스레드 시작
+		ConsumerThread th = new ConsumerThread(bar);
+		th.start();
+		
+		
 	}
 	
+
 	public static void main(String[] args) {
-		new TabAndThreadEx("아무키나 빨리 눌러 바 채우기");
+		// TODO Auto-generated method stub
+		new TabAndThreadEx("아무나 빨리 눌러 바 채우기");
 	}
 
 }
